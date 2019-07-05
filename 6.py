942482a751c020fd123e8780bddef1ed653022cc
devibala813 <51949237+devibala813@users.noreply.github.com>
2019-07-05 22:37:19 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,10 @@
+m1,m2=map(str,input().split())
+if(len(m1)!=len(m2)):
+   print("no")
+else:
+   o1=[m1.count(i) for i in o1]
+   o2=[m2.count(i) for i in o2]
+if(o1==o2):
+   print("yes")
+else:
+   print("no")
