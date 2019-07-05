72f04425ccbc69ca327b946fa1c23fb006c95916
devibala813 <51949237+devibala813@users.noreply.github.com>
2019-07-05 22:50:58 +0530

    Update 6.py

--- 6.py
+++ 6.py
@@ -2,9 +2,9 @@ m1,m2=map(str,input().split())
 if(len(m1)!=len(m2)):
    print("no")
 else:
-   o1=[m1.count(i) for i in m1]
-   o2=[m2.count(i) for i in m2]
-if(o1==o2):
+   k1=[m1.count(i) for i in m1]
+   k2=[m2.count(i) for i in m2]
+if(k1==k2):
    print("yes")
 else:
    print("no")
