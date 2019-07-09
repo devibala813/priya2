3f3e658a065f9620b3a7d3c1c150f6304c2713fa
devibala813 <51949237+devibala813@users.noreply.github.com>
2019-07-09 14:53:50 +0530

    Update h1.py

--- h1.py
+++ h1.py
@@ -1,9 +1,9 @@
 num1=int(input())
 l1=list(map(int,input().split()[:num1]))
-c=0
+count=0
 fp=[]
 for i in range(0,num1+1):
-   if(l1.c(i)>1):
+   if(l1.count(i)>1):
       fp.append(i)
 if(len(fp)==0):
    print("unique")
