6b4237321627303a660cf79261318cbb52d3370d
devibala813 <51949237+devibala813@users.noreply.github.com>
2019-07-09 14:44:23 +0530

    Create h1.py

---
+++ h1.py
@@ -0,0 +1,11 @@
+num1=int(input())
+l1=list(map(int,input().split()[:num]))
+c=0
+fp=[]
+for i in range(0,num+1):
+   if(l1.c(i)>1):
+      fp.append(i)
+if(len(fp)==0):
+   print("unique")
+kl=sorted(fp)
+print(' '.join(map(str,kl)))
